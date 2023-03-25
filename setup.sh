61d537ee8ef4546791a1051c5465f9772e2c0f51
satokiy <syamada@netprotections.co.jp>
2023-03-25 19:58:00 +0900

    setup sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# シンボリックリンク
+ln -sf ~/dotfiles/_vimrc/_vimrc ~/.vimrc # ディレクトリ構造は個別
+
+# vundleのclone
+git clone http://github.com/VundleVim/Vundle.Vim.git ~/dotfiles/vimfiles/.vim/vundle.git
+
+# vimエディタを開く
+vi
+
+# プラグインのインストール
+:BundleInstall
