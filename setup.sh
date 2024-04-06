017bb9a72fbf9f473dd789157ec8801bb18afc50
Satoki <43191933+satokiy@users.noreply.github.com>
2024-04-06 19:41:48 +0900

    refactor: remove unnecessary files

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
 
-# configディレクトリ配下の各ディレクトリでsetup.shを探し、存在する場合は実行する
 for setup_script in $(find ./config -name 'setup.sh'); do
     echo "Running $setup_script"
     chmod +x $setup_script
