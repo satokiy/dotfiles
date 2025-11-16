0f681981690fb99d4b646c4e546615618e768a18
Satoki Yamada <satoki.graffitti.1226@gmail.com>
2025-11-16 13:17:17 +0900

    wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,9 +7,14 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Solarized Dark'
+config.color_scheme = "Solarized Dark"
 config.window_background_opacity = 0.9
-config.font = wezterm.font('Fira Code')
-config.font_size = 14.0
--- and finally, return the configuration to wezterm
+
+config.keys = {
+	{
+		key = "-",
+		mods = "CTRL",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
 return config
